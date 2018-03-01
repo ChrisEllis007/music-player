e9931c9ef8f87855ec55512cff7e1b387d5bded0
chris.ellis <chris.ellis@fjordnet.com>
2018-03-02 10:50:04 +1100

    Create elements for songs and song lists

---
+++ src/components/song/song.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './song.css';
+
+
+class Song extends React.Component{
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return(
+            <div className='song'>
+                {this.props.boxArt &&
+                <img className='song-box-art' alt='box art' src={this.props.boxArt}/>
+                }
+                <div className="song-body">
+                    <h5 className="song-title">{this.props.songName}</h5>
+                    <h6 className="song-artist">{this.props.artist}</h6>
+                    <h6 className="song-album">{this.props.album }</h6>
+                </div>
+            </div>)
+    }
+}
+
+
+Song.propTypes = {
+    songName: PropTypes.string,
+    artist: PropTypes.string,
+    album: PropTypes.string
+}
+
+export default Song;
+
---
+++ src/components/songList/song-list.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Song from "../song/song";
+import './song-list.css';
+
+class SongList extends React.Component{
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return(
+        <ul className='songlist'>
+            {this.props.songs.map((song, index) => {
+               return(
+                <li key={"song"+index} >
+                    <Song
+                        boxArt={song.artworkUrl60}
+                        songName={song.trackName} artist={song.artistName} album={song.collectionName}></Song>
+                </li>)
+            })}
+        </ul>
+        )
+    }
+}
+
+
+SongList.propTypes = {
+    songs: PropTypes.array
+}
+
+export default SongList;
+
